73ee554497401eee65f5aea8d89f5015f778347d
iceeee <kenzo.lay1936@gmail.com>
2025-04-30 15:18:33 +0700

    fix: edit

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -23,12 +23,12 @@
         rel="stylesheet" />
     <link rel="stylesheet" href="{{asset('assets/vendor/fonts/boxicons.css')}}" />
     <link rel="stylesheet" href="{{asset('assets/vendor/css/core.css')}}" class="template-customizer-core-css" />
-    <link rel="stylesheet" href="{{asset('assets/vendor/css/theme-default.css" class="template-customizer-theme-css')}}" />
+    <link rel="stylesheet" href="{{asset('assets/vendor/css/theme-default.css')}}" class="template-customizer-theme-css" />
     <link rel="stylesheet" href="{{asset('assets/css/demo.css')}}" />
     <link rel="stylesheet" href="{{asset('assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css')}}" />
     <link rel="stylesheet" href="{{asset('assets/vendor/css/pages/page-auth.css')}}" />
-    <script src="asset('assets/vendor/js/helpers.js')"></script>
-    <script src="asset('assets/js/config.js')"></script>
+    <script src="{{asset('assets/vendor/js/helpers.js')}}"></script>
+    <script src="{{asset('assets/js/config.js')}}"></script>
 </head>
 
 <body>
